f3e50bd3fd55acd071e8aad0d9bf00a18a2c9eff
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-05 16:01:18 +0000

    Deploy yaeSakuras/impact to github.com/yaeSakuras/impact.git:gh-pages

---
+++ precache-manifest.946910a721e84c240ede96e253620816.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a85f57cb16197f621a44af1952f3e9",
+    "url": "/impact/index.html"
+  },
+  {
+    "revision": "c97e24e2e2b376c209a6",
+    "url": "/impact/static/css/4.8dc957de.chunk.css"
+  },
+  {
+    "revision": "26e2fea5c10053b8035a",
+    "url": "/impact/static/css/5.8dc957de.chunk.css"
+  },
+  {
+    "revision": "3ceccf99e5f05439f1a9",
+    "url": "/impact/static/css/6.00a8895b.chunk.css"
+  },
+  {
+    "revision": "dc4c41623938bbcee30b",
+    "url": "/impact/static/css/main.6811f87e.chunk.css"
+  },
+  {
+    "revision": "e06af397bcedf8e1a542",
+    "url": "/impact/static/js/0.049eceb8.chunk.js"
+  },
+  {
+    "revision": "30a04606b0b4f6f22ded",
+    "url": "/impact/static/js/10.f728171d.chunk.js"
+  },
+  {
+    "revision": "27b31910aa9a51062cbc",
+    "url": "/impact/static/js/3.cb775639.chunk.js"
+  },
+  {
+    "revision": "c97e24e2e2b376c209a6",
+    "url": "/impact/static/js/4.3ea23496.chunk.js"
+  },
+  {
+    "revision": "26e2fea5c10053b8035a",
+    "url": "/impact/static/js/5.062d2090.chunk.js"
+  },
+  {
+    "revision": "3ceccf99e5f05439f1a9",
+    "url": "/impact/static/js/6.64ac9a7b.chunk.js"
+  },
+  {
+    "revision": "8b10b03c87e92d7f2835",
+    "url": "/impact/static/js/7.2d853116.chunk.js"
+  },
+  {
+    "revision": "4ee17bcaf61dd07e9c7f",
+    "url": "/impact/static/js/8.de44a37e.chunk.js"
+  },
+  {
+    "revision": "c09b06cd0c3ca077017e",
+    "url": "/impact/static/js/9.24ece907.chunk.js"
+  },
+  {
+    "revision": "dc4c41623938bbcee30b",
+    "url": "/impact/static/js/main.ef8d9e47.chunk.js"
+  },
+  {
+    "revision": "54d12a37784b69259189",
+    "url": "/impact/static/js/runtime-main.edc0c3a2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-impact/precache-manifest.e1f82ff3e2049371e6899eaa8f1452f3.js"
+  "/impact/precache-manifest.946910a721e84c240ede96e253620816.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-impact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/impact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
