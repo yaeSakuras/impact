73bbccc802c157795ea30ceaba6ee1837c37adc6
yaeSakuras <363420113@qq.com>
2019-11-06 22:14:25 +0800

    chore: update title

--- src/layout/Article/index.js
+++ src/layout/Article/index.js
@@ -5,7 +5,7 @@ import CodeBlock from '@/components/Code';
 function Article(props) {
     return (
         <div className="z-article-content">
-            <div className="z-article-header">标题1</div>
+            <div className="z-article-header">标 题</div>
             <div className="z-article-body">
                 <div className="z-article-left z-markdown">
                     <Markdown source={props.md} renderers={{code: CodeBlock}}/>
